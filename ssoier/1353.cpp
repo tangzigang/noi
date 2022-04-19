b0fcdc03efcf0d7c34f52ed93e0da8bb7da8d64c
airgo <airgo.tang@gmail.com>
2022-04-19 18:59:32 +0800

    1353

---
+++ ssoier/1353.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+char s[256];
+char stack[20];
+int top = -1;
+int main(){
+
+    gets(s);
+    for(int i = 0; s[i] != '@' ;i++){
+        switch (s[i]){
+        case '(':
+            stack[++top] = s[i];
+            break;
+        case ')':
+            if(top < 0 || stack[top--] != '('){
+                cout << "NO"<<endl;
+                return 0;
+            };
+            break;
+        default:
+            break;
+        }
+    }
+
+    if(top == -1){
+        cout << "YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
