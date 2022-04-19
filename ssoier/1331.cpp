287522d7b597d1e7c8cebea69ff773779835676f
唐自刚 <airgo@tangzigangs-MacBook-Pro.local>
2022-04-19 18:41:22 +0800

    1331

---
+++ ssoier/1331.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+char s[256];
+long stack[256];
+int top = -1;
+
+long temp = 0;
+long n;
+int main(){
+    gets(s);
+
+    int i = 0;
+    char currentChar = s[0];
+    while(currentChar != '@'){
+        switch (currentChar)
+        {
+        case '+':
+            n = stack[top];
+            top--;
+            stack[top] = stack[top]+n;
+            break;
+        case '-':
+            n = stack[top];
+            top--;
+            stack[top] = stack[top]-n;
+            break;
+        case '*':
+            n = stack[top];
+            top--;
+            stack[top] = stack[top] * n;
+            break;
+        case '/':
+            n = stack[top];
+            top--;
+            stack[top] = stack[top] / n;
+            break;
+        case ' ':
+            top++;
+            stack[top] = temp;
+            temp = 0;
+            break;
+        default:
+            temp = temp * 10 + (currentChar - 48);
+            break;
+        }
+
+        i++;
+        currentChar = s[i];
+    }
+
+    cout << stack[0]<<endl;
+    
+    return 0;
+}
